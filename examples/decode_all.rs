d33bf36bccf1c72eb52aea5c5e32b6d66d46dff7
Ignacy Koper <ignacy423@gmail.com>
2024-08-28 22:55:44 +0200

    convert into a library
    Signed-off-by: Ignacy Koper <ignacy423@gmail.com>

--- examples/decode_all.rs
+++ examples/decode_all.rs
@@ -1,11 +1,9 @@
-mod decode;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let path: String = std::fs::read_to_string(
 		format!("{}/spec/amdgpu_isa_rdna3.xml", env!("CARGO_MANIFEST_DIR")
 	))?;
 
-	let spec: decode::Spec = quick_xml::de::from_str(path.as_str())?;
+	let spec: rdna3_isa::decode::Spec = quick_xml::de::from_str(path.as_str())?;
 
 	println!("{:#?}", spec);
 
